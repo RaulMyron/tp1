2b0e4b704a6753882142540694fbbad24b8d624a
Raul Myron <raul.myron@gmail.com>
2025-09-02 11:37:09 -0300

    Update 2.cpp

--- s1/2.cpp
+++ s1/2.cpp
@@ -7,27 +7,29 @@ int main()
 {
 
     float taxa;
-    int conta;
+    float conta;
 
     cin >> conta;
 
-    if (conta < 500)
+    if (conta <= 500)
     {
         taxa = 0.02;
         float total = conta * taxa;
-        cout << fixed << setprecision(2) << total << " " << fixed << setprecision(2) << 5.00 << " " << fixed << setprecision(2) << total + 5 << endl;
+        //cout << fixed << setprecision(2) << total << " " << fixed << setprecision(2) << 5.00 << " " << fixed << setprecision(2) << total + 5 << endl;
+        printf("%.2f %.2f %.2f", total, 5.00, total+5);
     }
-    else if (conta > 500 && conta < 1000)
+    else if (conta > 500 && conta <= 1000)
     {
         taxa = 0.1;
         float total = (500 * taxa) + (0.05 * (conta - 500));
-        cout << fixed << setprecision(2) << total << " " << fixed << setprecision(2) << 5.00 << " " << fixed << setprecision(2) << total + 5 << endl;
+        //cout << fixed << setprecision(2) << total << " " << fixed << setprecision(2) << 5.00 << " " << fixed << setprecision(2) << total + 5 << endl;
+        printf("%.2f %.2f %.2f", total, 5.00, total+5);
     }
     else
     {
         taxa = 0.35;
-        float total = (500 * taxa) + (0.1 * (conta - 1000));
-        cout << fixed << setprecision(2) << total << " " << fixed << setprecision(2) << 5.00 << " " << fixed << setprecision(2) << total + 5 << endl;
+        float total = (1000 * taxa) + (0.1 * (conta - 1000));
+        printf("%.2f %.2f %.2f", total, 5.00, total+5);
     }
 
     return 0;
